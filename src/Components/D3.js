467292c8a207ce168d14b860acea7af43c6c8e96
abhi199kr <abhishek@appsdeployer.com>
2022-10-31 16:45:36 +0530

    image

--- src/Components/D3.js
+++ src/Components/D3.js
@@ -10,31 +10,34 @@ const D3 = () => {
       <p className='bl text-center '>Not all domain brokers are created equal. With more than a decade of experience in the industry, having helped clients acquire and sell some of the more exclusive domain names in the world, we have gained a unique set of skills, knowledge, and experience, and have built a powerful network that allows us to help you reach your goals. Don't take our word for it; check out what some of our clients have said in the testimonials.
       </p>
 
-      <div className='row re'>
-      {/* <div className="col-md-3 hc">
-       <img src="https://apexmoon.com/img/global-expert.svg" className='wi' alt="" />
-        <p>Global Expert</p>
-      </div> */}
-      <div className="col-md-3 -pt-2">
-      <img className='wi' src="https://apexmoon.com/img/global-expert.svg" alt="" />
-      <p>Global Expert</p>
-      </div>
-      <div className="col-md-3 hc">
-        <img src="https://apexmoon.com/img/Trustworthy.svg" className='wi' alt="" />
-        <p>Global Expert</p>
-      </div>
-      <div className="col-md-3 hc">
-        <img src="https://apexmoon.com/img/Proven-Success.svg" className='wi' alt="" />
-        <p>Global Expert</p>
-      </div>
-      <div className="col-md-3 hc">
-        <img src="https://apexmoon.com/img/Confidentiality.svg" className='wi' alt="" />
-        <p>Global Expert</p>
-      </div>
+      <div className='row my-5 re'>
       
-      {/* <div className="col-md-3 hc">dfdfffd</div>
-      <div className="col-md-3 hc">dfdfffd</div>
-      <div className="col-md-3 hc">dfdfffd</div> */}
+      <div className="col-md-3 -pt-2">
+      <div className=' a'>
+      <p className='text-inline'>Global Expert</p>
+      </div>
+      </div>
+      <div className="col-md-3 -pt-2">
+      <div className=' b'>
+      <p className='text-inline'>Global Expert</p>
+      </div>
+      </div>
+
+      <div className="col-md-3 -pt-2">
+      <div className=' c'>
+      <p className='text-inline'>Global Expert</p>
+      </div>
+      </div>
+
+      <div className="col-md-3 -pt-2">
+      <div className=' d'>
+      <p className='text-inline'>Global Expert</p>
+      </div>
+      </div>
+     
+      
+      
+    
 
       </div>
 
